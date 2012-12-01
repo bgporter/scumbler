d88170d4bd831b88f7ea493d2c9a7d47b28c9540
Brett g Porter <bgp@bgporter.net>
2012-12-01 16:04:52 -0500

    Adding (empty) source files for the Track class

---
+++ Source/Track.cpp
@@ -0,0 +1,3 @@
+// Copyright (c) 2012 Bg Porter
+
+#include "Track.h"
---
+++ Source/Track.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2012 Bg Porter.
+
+// !!! License
+
+#ifndef h_Track
+#define h_Track
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+
+
+#endif // this must be the last line in this file.
