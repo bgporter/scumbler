fef798eadb29edcce577b9d6198432da906d0acc
Brett g Porter <bgp@bgporter.net>
2012-12-01 15:50:21 -0500

    Changed alignment
    (really just testing new alignment plugin in SublimeText2)

--- Source/Commands.h
+++ Source/Commands.h
@@ -7,15 +7,15 @@
 
 namespace CommandIds
 {
-    static const CommandID kNew     = 0x2FFF;
-    static const CommandID kOpen    = 0x3000;
-    static const CommandID kSave    = 0x3001;
-    static const CommandID kSaveAs  = 0x3002;
-    static const CommandID kConfigAudio = 0x3003;
-    static const CommandID kAboutBox = 0x3004;
-    static const CommandID kPlay    = 0x3005;
-    static const CommandID kPause   = 0x3006;
-    static const CommandID kRewind  = 0x3007;
+    static const CommandID kNew          = 0x2FFF;
+    static const CommandID kOpen         = 0x3000;
+    static const CommandID kSave         = 0x3001;
+    static const CommandID kSaveAs       = 0x3002;
+    static const CommandID kConfigAudio  = 0x3003;
+    static const CommandID kAboutBox     = 0x3004;
+    static const CommandID kPlay         = 0x3005;
+    static const CommandID kPause        = 0x3006;
+    static const CommandID kRewind       = 0x3007;
     static const CommandID kToggleRecord = 0x3008;      
 }
 
