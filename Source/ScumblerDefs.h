5a19bb2f7fd48040a5ca325b7048dbafa08eb5f9
Brett g Porter <bgp@bgporter.net>
2012-12-02 13:45:14 -0500

    Adding header file to hold project-wide definitions
    Currently only holding the #define that controls the
    inclusion of unit tests.

---
+++ Source/ScumblerDefs.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2012 Bg Porter.
+
+// !!! License
+
+#ifndef h_ScumblerDefs
+#define h_ScumblerDefs
+
+// Later, we can set this to conditionally compile in only on debug 
+// builds if we like.
+#define qUnitTests
+
+
+
+#endif // this must be the last line in this file.
