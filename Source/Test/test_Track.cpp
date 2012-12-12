06a6db4d43f156afad28c3b86667501dd3e85cc4
Brett g Porter <bgp@bgporter.net>
2012-12-12 11:10:47 -0500

    adding (empty) test file for the Track class.

---
+++ Source/Test/test_Track.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2012 Bg Porter
+
+// NOTE that this file shouldn't be included in a project from the 
+// compiler's point of view; when we're compiled with unit tests 
+// enabled, this file should be #included into the associated file 
+// in the directory above this one. We're only breaking this out into 
+// a separate file to avoid cluttering our file under test.
+
+class TrackTest : public UnitTest
+{
+public:
+   TrackTest() : UnitTest("Track unit tests")
+   {
+
+   }
+
+
+   void initialise()
+   {
+   
+   };
+
+   void shutdown()
+   {
+   
+   };
+
+   void runTest()
+   {
+   }
+
+private:
+
+};
