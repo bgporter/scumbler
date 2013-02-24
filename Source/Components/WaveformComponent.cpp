9c62419cbf8055aa97e08527a0792d1ad5cc4bb4
Brett g Porter <bgp@bgporter.net>
2013-02-24 08:45:05 -0500

    Adding Waveform component.
    At the moment, doesn't do anything except draw an X where the waveform display
    will end up being drawn.

---
+++ Source/Components/WaveformComponent.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2013 Bg Porter
+
+#include "WaveformComponent.h"
+
+#include "Track.h"
+
+WaveformComponent::WaveformComponent(Track* track)
+:  fTrack(track),
+   fLoop(nullptr)
+{
+   if (fTrack)
+   {
+      this->ConnectToLoop(fTrack->GetLoop());
+   }
+}
+
+WaveformComponent::~WaveformComponent()
+{
+
+}
+
+void WaveformComponent::ConnectToLoop(LoopProcessor* loop)
+{
+   if (loop != fLoop)
+   {
+      if (fLoop)
+      {
+         fLoop->removeChangeListener(this);
+      }
+      fLoop = loop;
+      if (fLoop)
+      {
+         fLoop->addChangeListener(this);
+      }
+   }   
+}
+
+void WaveformComponent::changeListenerCallback(ChangeBroadcaster* source)
+{
+   //!!! Not sure how we'll handle this. 
+
+}
+
+void WaveformComponent::resized()
+{
+
+}
+
+void WaveformComponent::paint(Graphics& g)
+{
+   // !!! for now, we'll just draw the bounds and a big X as a placeholder.
+   g.setColour(Colours::black);
+   g.drawRect(0, 0, this->getWidth(), this->getHeight());
+   g.drawLine(0, 0, this->getWidth(), this->getHeight());
+   g.drawLine(0, this->getHeight(), this->getWidth(), 0);
+
+}
---
+++ Source/Components/WaveformComponent.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2013 Bg Porter
+
+// !!! License
+
+#ifndef h_WaveformComponent
+#define h_WaveformComponent
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+class LoopProcessor;
+class Track;
+
+class WaveformComponent :  public Component
+                        ,  public ChangeListener   
+{
+public:
+   WaveformComponent(Track* track);
+
+   ~WaveformComponent();
+
+   /**
+    * Connect at runtime to a specific loop processor. This is useful because 
+    * a particular TrackComponent (and its children) may be connected to several different 
+    * Track objects over its lifetime.
+    * @param loop Pointer to the LoopProcessor object that holds the samples we 
+    * need to display. If nullptr, we disconnect.
+    */
+   void ConnectToLoop(LoopProcessor* loop);
+
+   /**
+    * Called by the loop processor when we need to know that something has changed.
+    * @param source [description]
+    */
+   void changeListenerCallback(ChangeBroadcaster* source);
+
+   /**
+    * Called by JUCE when our size changes.
+    */
+   void resized();
+
+   /**
+    * Called by JUCE when we need to paint ourself.
+    * @param g Graphics object to use to draw.
+    */
+   void paint(Graphics& g);
+
+
+private:
+   Track* fTrack;
+   LoopProcessor* fLoop;
+
+};
+
+#endif
