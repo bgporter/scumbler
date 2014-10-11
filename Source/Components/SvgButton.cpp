eeb2a4432a47a9b9d51df69f9f5ccd21a4b08520
Brett g Porter <bgp@bgporter.net>
2014-10-11 11:24:19 -0400

    initial commit, SvgButton class.

---
+++ Source/Components/SvgButton.cpp
@@ -0,0 +1,74 @@
+// Copyright (c) 2014 Brett g Porter
+
+#include "SvgButton.h"
+
+#include "UiStyle.h"
+
+
+namespace
+{
+   struct SvgColor
+   {
+     String rgb;
+     String opacity;
+   };
+
+   const uint32 kRgbMask(0x00FFFFFF);
+
+   void ArgbToSvg(Colour c, SvgColor* svg)
+   {
+     uint32 argb = c.getARGB();
+     uint32 rgb = argb & kRgbMask;
+     float alpha = c.getFloatAlpha();
+
+     svg->rgb = String::toHexString(static_cast<int>(rgb)).paddedLeft('0', 6);
+     svg->opacity = String(alpha);
+
+   }
+}
+
+
+SvgButton::SvgButton(const String& source)
+:fSource(source)
+{
+   
+}
+
+SvgButton::~SvgButton()
+{
+   
+}
+
+void SvgButton::SetPaletteEntry(const String& svgKey, const String& paletteKey)
+{
+   fColorTable[svgKey] = paletteKey;
+}
+
+Drawable* SvgButton::Create(UiStyle* style)
+{
+   // make a writable copy...
+   String svg(fSource);
+
+   std::map<String, String>::iterator it;
+
+   // walk through each color table association and replace the template items 
+   // with correctly formatted color data.
+   for (it = fColorTable.begin(); it != fColorTable.end(); ++it)
+   {
+      String svgItem = it->first;
+      String palette = it->second;
+
+      SvgColor color;
+
+      ArgbToSvg(style->GetColor(palette), &color);
+
+      svg = svg.replace("{" + svgItem + "}", color.rgb);
+      svg = svg.replace("{" + svgItem + "-opacity}", color.opacity);
+   }
+
+   XmlElement* x = XmlDocument::parse(svg);
+   Drawable* retval = Drawable::createFromSVG(*x);
+   delete x;
+   return retval;
+   
+}
---
+++ Source/Components/SvgButton.h
@@ -0,0 +1,79 @@
+// Copyright (c) 2014 Bg Porter
+
+// !!! license
+
+#ifndef h_SvgButton
+#define h_SvgButton
+
+#include "JuceHeader.h"
+
+#include <map>
+
+// forward ref
+class UiStyle;
+
+/**
+ * \class SvgButton
+ *
+ *  We want to be able to use SVG to define the design of our flat buttons. This class 
+ *  lets us mark up SVG data to use as a template that can be styled at runtime using 
+ *  palette data. 
+ *
+ *  Each button should have at least 4 palette colors associated with it:
+ *  - bg-stroke (border)
+ *  - bg-fill   (button fill color)
+ *  - fg-stroke (shape/letter outline)
+ *  - fg-fill   (shape/letter fill)
+ *
+ * (Note that in many cases, we'll expect the fg stroke and fill colors to be the same)
+ * We mark these up in the SVG string like this:
+ *  `<polygon points='6,12 11,8 6,4   ' style='fill:#{fg-fill};fill-opacity:{fg-fill-opacity}`
+ *
+ * Also note that SVG keeps color and opacity/alpha data separately, so for each of the colors
+ * in the list above, there are both e.g. `bg-stroke` and `bg-stroke-opacity` template
+ * placeholders.
+ *
+ * We keep the associations between the SVG entries and our palette identifiers in 
+ * a map to adapt for future cases where there are more elements to be colored inside the
+ * button (and these things may not actually *be* buttons, I guess. What this class really lets us
+ * manage is palette-stylable SVG drawables.) 
+ * 
+ */
+class SvgButton
+{
+public:
+   /**
+    * Create the object, keeping a const version of the original string.
+    */
+   SvgButton(const String& source);
+
+   ~SvgButton();
+
+   /**
+    * Create an association between one of our template keys and a palette entry.
+    * This should only need to be done once at startup time for each of these objects.
+    * @param svgKey     e.g. 'bg-stroke'
+    * @param paletteKey e.g. 'TransportPlayHoverBorder'
+    */
+   void SetPaletteEntry(const String& svgKey, const String& paletteKey);   
+
+   /**
+    * Create a new drawable object that uses current palette data from the UiStyle object
+    * that we are passed.
+    * @param  style UiStyle object with the active palette
+    * @return       Pointer to a Drawable object that we can use. It's the caller's 
+    *                       responsibility to clean this up.
+    */
+   Drawable* Create(UiStyle* style);
+
+
+
+private:
+   // original marked-up SVG data that we'll update with palette data at runtime.
+   const String fSource;
+
+   std::map<String, String> fColorTable;
+};
+
+
+#endif
