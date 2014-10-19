95af25f2b84699e7579053700b0af10f9edc2426
Brett g Porter <bgp@bgporter.net>
2014-10-19 11:40:38 -0400

    thickened border stroke width

--- Source/Components/Buttons/PlayButton.h
+++ Source/Components/Buttons/PlayButton.h
@@ -13,7 +13,7 @@ const String kPlayButton("<?xml version='1.0' standalone='no'?>\
 <svg version='1.1' baseProfile='full' xmlns='http://www.w3.org/2000/svg' \
    xmlns:xlink='http://www.w3.org/1999/xlink'>\
    <ellipse cx='32' cy='32' rx='32' ry='32' \
-      style='fill:#{bg-fill};fill-opacity:{bg-fill-opacity};stroke:#{bg-stroke};stroke-opacity:{bg-stroke-opacity};stroke-width:3'/>\
+      style='fill:#{bg-fill};fill-opacity:{bg-fill-opacity};stroke:#{bg-stroke};stroke-opacity:{bg-stroke-opacity};stroke-width:6'/>\
    <polygon points='24,48 44,32 24,16   '\
       style='stroke:#{fg-stroke};stroke-opacity:{fg-stroke-opacity};fill:#{fg-fill};fill-opacity:{fg-fill-opacity}'/>\
 </svg>");
@@ -24,7 +24,7 @@ const String kPauseButton("<?xml version='1.0' standalone='no'?>\
 <svg version='1.1' baseProfile='full' xmlns='http://www.w3.org/2000/svg' \
    xmlns:xlink='http://www.w3.org/1999/xlink'>\
    <ellipse cx='32' cy='32' rx='32' ry='32' \
-      style='fill:#{bg-fill};fill-opacity:{bg-fill-opacity};stroke:#{bg-stroke};stroke-opacity:{bg-stroke-opacity};stroke-width:3'/>\
+      style='fill:#{bg-fill};fill-opacity:{bg-fill-opacity};stroke:#{bg-stroke};stroke-opacity:{bg-stroke-opacity};stroke-width:6'/>\
    <rect x='20' y='12' width='6' height='40' style='fill:#{fg-fill};fill-opacity:{fg-fill-opacity};stroke:#{fg-stroke};stroke-opacity:fg-stroke-opacity}'/>\
    <rect x='36' y='12' width='6' height='40' style='fill:#{fg-fill};fill-opacity:{fg-fill-opacity};stroke:#{fg-stroke};stroke-opacity:fg-stroke-opacity}'/>\
 </svg>");
